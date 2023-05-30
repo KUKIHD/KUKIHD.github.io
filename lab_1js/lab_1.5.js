0e75a3d0eb6b7a4c8c9d474d4828f3df661a26da
KUKIHD <106991810+KUKIHD@users.noreply.github.com>
2023-05-31 02:37:17 +0300

    Update lab_1.5.js

--- lab_1js/lab_1.5.js
+++ lab_1js/lab_1.5.js
@@ -1,1 +1,28 @@
 'use strict';
+
+let num = prompt("Введите число");
+let num1 = ("dog dog dog cat cat dog");
+let num2 = ("dog dog cat cat dog dog");
+let num3 = ("dog cat cat dog dog dog");
+let num4 = ("cat cat dog dog dog dog");
+let num5 = ("dog dog dog dog cat cat");
+let num0 = 1;
+let num6 = Number(num);
+do {
+  if (num0 % 5 == 1){
+    console.log(num1)
+  }
+  if (num0 % 5 == 2){
+    console.log(num2)
+  }
+  if (num0 % 5 == 3){
+    console.log(num3)
+  }
+  if (num0 % 5 == 4){
+    console.log(num4)
+  }
+  if (num0 % 5 == 0){
+    console.log(num5)
+  }
+  num0 = num0 + 1;
+} while (num0 <= num6)
