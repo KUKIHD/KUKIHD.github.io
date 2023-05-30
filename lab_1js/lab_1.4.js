9f41b1db6149565dddff22c42d9fddf840e3e304
KUKIHD <106991810+KUKIHD@users.noreply.github.com>
2023-05-31 02:39:26 +0300

    Update lab_1.4.js

--- lab_1js/lab_1.4.js
+++ lab_1js/lab_1.4.js
@@ -1,12 +1,10 @@
 'use strict';
 
 let num = prompt("Введите любое число");
-let otvet = [];
 next:
 for ( let a = 2; a <= num; a++){
   for ( let b = 2; b < a; b++){
     if (a % b == 0) continue next;
   }
-  otvet = (`${otvet + a}  `);
+  console.log(a);
 }
-alert(otvet);
