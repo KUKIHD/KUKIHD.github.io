c71096f8eb65bfc37de5b8d2d01091ab7b406b1c
KUKIHD <106991810+KUKIHD@users.noreply.github.com>
2023-06-03 22:37:07 +0300

    Update lab_5.1.js

--- lab_5js/lab_5.1.js
+++ lab_5js/lab_5.1.js
@@ -1,6 +1,6 @@
 const head = document.head;
 const ul = document.querySelector('ul');
-const li = ul.querySelectorAll('li');
+const lil = ul.querySelectorAll('li');
 console.log(head)
 console.log(ul)
-console.log(li)
+console.log(lil)
