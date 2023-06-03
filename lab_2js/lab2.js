1ef99cd9930d18b6a1c4d0e323bf69132435c22b
KUKIHD <106991810+KUKIHD@users.noreply.github.com>
2023-06-03 21:01:14 +0300

    Update lab2.js

--- lab_2js/lab2.js
+++ lab_2js/lab2.js
@@ -1,5 +1,9 @@
-"use strict";
-
+/**
+ * Возвращает x, возведённое в n-ную степень.
+ * @param {number} x Возводимое в степень число.
+ * @param {number} n Степень, должна быть натуральным числом.
+ * @return {number} x ^ n, возведённое в n-ную степень.
+ */
 function pow(x,n){
   if (n == 0){
     return 1;
@@ -12,6 +16,11 @@ function pow(x,n){
   }
 }
 
+/**
+ * Возвращает сумму чисел от 1 до n включительно.
+ * @param {number} n Число, до которого вычисляется сумма, должно быть натуральным числом.
+ * @return {number} суммирует число n и возвращает функцию(n-1).
+ */
 function sumTo(n){
   if (n == 1){
     return 1;
@@ -21,6 +30,11 @@ function sumTo(n){
   }
 }
 
+/**
+ * Возвращает факториал числа n!.
+ * @param {number} n Число, факториал которого вычисляется, должно быть натуральным числом.
+ * @return {number} умножает число n на функцию(n-1).
+ */
 function factorial(n){
   if (n == 0){
     return 1n;
@@ -30,6 +44,11 @@ function factorial(n){
   }
 }
 
+/**
+ * Возвращает n-е число Фибоначчи.
+ * @param {number} n Порядковый номер числа Фибоначчи, должен быть натуральным числом.
+ * @return {number} y, n-е число Фибоначчи.
+ */
 function fib(n){
   let x = 0n;
   let y = 1n;
@@ -49,7 +68,11 @@ function fib(n){
         return y;
   }
 }
-
+/**
+ * Возвращает результат сравнения значений y и x.
+ * @param {number} x Первый аргумент.
+ * @param {number} y Второй аргумент.
+ * @return {function} Функция, которая вернет true, false или null.
 function compare(x){
   return function (y){
     if (x == y){
@@ -64,6 +87,11 @@ function compare(x){
   }
 }
 
+/**
+ * Возвращает сумму всех своих аргументов.
+ * @param {number} Значения аргументов.
+ * @return {number} Сумма всех своих аргументов.
+ */
 function sum(...args) {
     return args.reduce((acc, curr) => acc + curr, 0);
 }
