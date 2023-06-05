1b1a5a8e72cbae1a965d8ed3376ab6c19d2aabdf
KUKIHD <106991810+KUKIHD@users.noreply.github.com>
2023-06-06 00:15:37 +0300

    Update lab_1.2.js

--- lab_1js/lab_1.2.js
+++ lab_1js/lab_1.2.js
@@ -1,5 +1,3 @@
-'use strict';
-
 let num = +prompt("Количество ворон");
 if (num == 1){ 
   alert(`На ветке сидит ${num} ворона`);
@@ -7,6 +5,18 @@ if (num == 1){
 else if (num >= 2 && num <= 4){
   alert(`На ветке сидит ${num} вороны`);
 }
-else if (num >= 5){
+else if (num >= 5 && num < 20){
   alert(`На ветке сидит ${num} ворон`);
+}
+else if (num >= 20){
+  let ost = num % 10;
+  if (ost == 1){ 
+  alert(`На ветке сидит ${num} ворона`);
+}
+else if (ost >= 2 && ost <= 4){
+  alert(`На ветке сидит ${num} вороны`);
+}
+else if (ost >= 5 && ost < 20){
+  alert(`На ветке сидит ${num} ворон`);
+}
 }
