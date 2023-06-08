d71b678e067bb3e4f1734ca2c1e70a60b2a80876
KUKIHD <106991810+KUKIHD@users.noreply.github.com>
2023-06-08 20:30:14 +0300

    Update lab_7.2.js

--- lab_7js/lab_7.2.js
+++ lab_7js/lab_7.2.js
@@ -3,13 +3,13 @@ let tf = document.getElementById("hhh");
 let input = tf.getElementsByTagName("input")[0];
 let lb = tf.getElementsByTagName("label")[0];
 lb.style.display = "none";
-input.setAttribute("placeholder", input.dataset.placeholder); //setAttribute добавляет атрибут или изменяет значение существующего атрибута в выбранном элементе.
+input.setAttribute("placeholder", input.dataset.placeholder);
 lb.innerHTML = input.dataset.placeholder;
-input.addEventListener("blur", function () {
+input.addEventListener("blur", function (){
 lb.style.display = "none";
 input.setAttribute("placeholder", input.dataset.placeholder);
 });
-input.addEventListener("focus", function () { //addEventListener устанавливает функцию, которая будет вызываться всякий раз, когда указанное событие доставляется цели
+input.addEventListener("focus", function (){
 lb.style.display = "block";
 input.setAttribute("placeholder", "");
 });
